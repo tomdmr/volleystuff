d7af2818525b155ecfcb62b5f24ceac4d827bd17
tom_(Laptop) <thd@mnet-online.de>
2021-10-10 15:59:37 +0200

    Added btnArray for easier array access

--- js/dentry.js
+++ js/dentry.js
@@ -12,8 +12,57 @@ let rotT = [0,0];
 let srvT = 0;
 let ptsT = [];
 
+let btnArray = [];
+
 function initDentry(){
     console.log('Into DEntry()');
+    btnArray = [
+        [
+            document.getElementById('bt00'),
+            document.getElementById('bt01'),
+            document.getElementById('bt02'),
+            document.getElementById('bt03'),
+            document.getElementById('bt04'),
+            document.getElementById('bt05'),
+            document.getElementById('bt06'),
+            document.getElementById('bt07'),
+        ],
+        [
+            document.getElementById('bt10'),
+            document.getElementById('bt11'),
+            document.getElementById('bt12'),
+            document.getElementById('bt13'),
+            document.getElementById('bt14'),
+            document.getElementById('bt15'),
+            document.getElementById('bt16'),
+        ],
+        [
+            document.getElementById('bt20'),
+            document.getElementById('bt21'),
+            document.getElementById('bt22'),
+            document.getElementById('bt23'),
+            document.getElementById('bt24'),
+            document.getElementById('bt25'),
+        ],
+        [
+            document.getElementById('bt30'),
+            document.getElementById('bt31'),
+            document.getElementById('bt32'),
+            document.getElementById('bt33'),
+            document.getElementById('bt34'),
+            document.getElementById('bt35'),
+            document.getElementById('bt36'),
+        ],
+        [
+            document.getElementById('bt40'),
+            document.getElementById('bt41'),
+            document.getElementById('bt42'),
+            document.getElementById('bt43'),
+            document.getElementById('bt44'),
+            document.getElementById('bt45'),
+            document.getElementById('bt46'),
+        ]
+    ];
     sctBox = document.getElementById('sctBox');
     plrBtT0 = [
         document.getElementById('bt00'),
@@ -74,8 +123,12 @@ function onSkill  (skill){
     window.navigator.vibrate(0);
 }
 function onType   (type){
+    //let std = ['H', 'M', 'Q', 'T', 'U', 'F', 'O'];
     console.log(type);
     sctBox.value += type;
+    ['H', 'M', 'Q', 'T', 'U', 'F', 'O'].forEach(function(val, idx){
+        btnArray[3][idx].value = val;
+    })
 }
 function onQuality(qual){
     sctBox.value += qual;
@@ -85,6 +138,7 @@ function onChain(){
     isChain = true;
 }
 function setServ(team){
+    let eve=['Standing','Jmp Flt', 'Jmp Rot', 'T', 'U', 'F', 'Other'];
     plrBt.forEach(function(x,t){
         x.forEach(function(item,idx){
             let cl = (idx>0) && (idx<4) ? 'bg-blue-front': 'bg-blue-back';
@@ -95,6 +149,9 @@ function setServ(team){
     plrBt[team][rotT[team]].classList.add('border-red')
     sctBox.value = (srvT==0?'*':'a')+plrBt[team][rotT[team]].value + 's';
     ptsT[0].value = ptsT[1].value = 0;
+    ['Standing','Jmp Flt', 'Jmp Rot', 'T', 'U', 'F', 'Other'].forEach(function(val, idx){
+        btnArray[3][idx].value = val;
+    })
 
 }
 function onPoint(team){
@@ -130,7 +187,6 @@ function transferT0(){
         let p = document.getElementById('pa'+i).value;
         if( p>0){
             let t = document.getElementById('a'+i).value;
-            console.log('Have a position ' + p + ' as ' + t);
             plrBt[0][p-1].value = t;
         }
     }
@@ -138,7 +194,6 @@ function transferT0(){
         let p = document.getElementById('pb'+i).value;
         if( p>0){
             let t = document.getElementById('b'+i).value;
-            console.log('Have a position ' + p + ' as ' + t);
             plrBt[1][p-1].value = t;
         }
     }
