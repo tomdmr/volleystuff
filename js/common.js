99ad06fb1d992628a70ce184a38567387e84a19a
tom_(Laptop) <thd@mnet-online.de>
2021-11-01 10:40:16 +0100

    Added vmin etc for javascript

--- js/common.js
+++ js/common.js
@@ -20,3 +20,21 @@ function toggleVisibility(elem){
     else
         el.style.display= 'none';
 }
+
+function vh(v) {
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  return (v * h) / 100;
+}
+
+function vw(v) {
+  var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+  return (v * w) / 100;
+}
+
+function vmin(v) {
+  return Math.min(vh(v), vw(v));
+}
+
+function vmax(v) {
+  return Math.max(vh(v), vw(v));
+}
